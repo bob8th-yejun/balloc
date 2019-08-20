8b1d6395d89977924eed541fb2522b26e7a32c16
신예준 <wns1826@naver.com>
2019-08-21 04:22:59 +0900

    bin struct 추가

--- alloc.c
+++ alloc.c
@@ -5,7 +5,32 @@
 extern void debug(const char *fmt, ...);
 extern void *sbrk(intptr_t increment);
 
+typedef struct fastBin{
+    void* next;
+}fastBin;
+
+typedef struct binHeader{
+    uint32_t size;
+    void *next;
+}binHeader;
+
+typedef struct sortBinHeader{
+    uint32_t size;
+    void *next;
+    void *prev;
+    void *index;
+}sortBinHeader;
+
+typedef struct binFooter{
+    uint32_t size;
+}binFooter;
+
 unsigned int max_size;
+void *fastbin_head, *fastbin_tail;
+void *smallbin_head, *smallbin_tail;
+void *unsortbin_root, *unsortbin_tail;
+void *sortbin_head, *sortbin_tail, *sortbin_index;
+unsigned int sortbin_count, sortbin_indexsize;
 
 void *myalloc(size_t size)
 {
