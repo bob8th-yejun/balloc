a0ccff52106bc8aaad8240937d7391ed13d9b2ad
신예준 <wns1826@naver.com>
2019-08-22 02:55:35 +0900

    0822 0255

--- alloc.c
+++ alloc.c
@@ -2,12 +2,16 @@
 #include <stdlib.h>
 #include <string.h>
 
+#define DEBUG
+
+#ifdef DEBUG
 extern void debug(const char *fmt, ...);
+#endif
 extern void *sbrk(intptr_t increment);
 
 typedef struct fastbin{
     void* next;
-}fastBin;
+}fastbin;
 
 typedef struct binHeader{
     uint32_t size;
@@ -19,31 +23,55 @@ typedef struct sortbinHeader{
     void *next;
     void *prev;
     void *index;
-}sortBinHeader;
+}sortbinHeader;
 
 typedef struct binFooter{
     uint32_t size;
 }binFooter;
 
+#ifdef DEBUG
 unsigned int max_size;
+#endif
 void *fastbin_head, *fastbin_tail;
 void *smallbin_head, *smallbin_tail;
 void *unsortbin_root, *unsortbin_tail;
 void *sortbin_head, *sortbin_tail, *sortbin_index;
 unsigned int sortbin_count, sortbin_indexsize;
 
-void sortSortbin() {
+void indexSortbin() {
     ;
 }
-void unsortSortbin() {
+void mvindexSortbin() {
+    ;
+}
+void unindexSortbin() {
     ;
 }
 
 void pushFastbin(void *ptr) {
-    ;
+#ifdef DEBUG
+    debug("[+] pushFastbin : ptr(%p)\n", ptr);
+    void *oldtail = fastbin_tail;
+#endif
+    if (fastbin_head == NULL) {
+        fastbin_head = fastbin_tail = ptr;
+#ifdef DEBUG
+        debug("[=] pushFastbin : head(%p) & tail(%p) set\n\n", fastbin_head, fastbin_tail);
+#endif
+    }
+    else {
+        ((fastbin *)fastbin_tail)->next = ptr;
+        ((fastbin *)ptr)->next = NULL;
+        fastbin_tail = ptr;
+#ifdef DEBUG
+        debug("[=] pushFastbin : oldtail(%p {next(%p)}) -> tail(%p)\n\n", oldtail, ((fastbin*)oldtail)->next, fastbin_tail);
+#endif
+    }
 }
 void *popFastbin() {
-    ;
+    // void *p = fastbin_head;
+    // fastbin_head = ((fastbin *)fastbin_head)->next;
+    // return p;
 }
 
 void pushSmallbin(void *ptr, uint32_t size) {
@@ -63,64 +91,88 @@ void *popUnsortbin(uint32_t size) {
 void pushSortbin(void *ptr, uint32_t size) {
     ;
 }
-void *pushSortbin(void size) {
+void *popSortbin(uint32_t size) {
     ;
 }
 
 void pushNode(void *ptr, uint32_t size) {
-    if (size == 8) { //fastbin
-        ;
-    }
-    else if (size < 32) { //smallbin
-        ;
-    }
-    else { //unsortbin or sortbin
-        ;
-    }
+    // if (size == 8)
+    //     pushFastbin(ptr);
+    // else if (size < 32)
+    //     pushSmallbin(ptr, size);
+    // else
+    //     pushSortbin(ptr, size);
+    pushFastbin(ptr);
 }
 
-void popNode(uint32_t size) {
-    if (size == 8) { //fastbin
-        ;
+void* popNode(uint32_t size) {
+    void *p = NULL;
+    if (size == 8) {
+        popFastbin(size);
     }
-    else if (size < 32) { //smallbin
-        ;
+    else if (size < 32) {
+        popSmallbin(size);
     }
-    else { //unsortbin or sortbin
-        ;
+    else {
+        popSortbin(size);
     }
+    return p;
 }
 
 void *myalloc(uint32_t size)
 {
+   // printf("[+] ");
+#ifdef DEBUG
+    debug("[+] myalloc : size(%d)\n", size);
+#endif
+    if (size == 0)
+        return NULL;
     size = ((size + 11) >> 3) << 3;
-    void *p = popNode();
-    if (p == NULL)
-        p = sbrk(size);
-    debug("alloc(%u): %p\n", (unsigned int)size, p);
-    max_size += size;
-    debug("max: %u\n", max_size);
-    return p;
-}
 
-void *myrealloc(void *ptr, uint32_t size)
-{
-    size = ((size +7) >> 3) << 3;
-    void *p = NULL;
-    if (size != 0)
-    {
-        p = myalloc(size);
-        if (ptr)
-            memcpy(p, ptr, size);
-        max_size += size;
-        debug("max: %u\n", max_size);
+    void *p = popNode(size);
+    if (p != NULL) {
+        ;
     }
-    debug("realloc(%p, %u): %p\n", ptr, (unsigned int)size, p);
-    return p;
+    else {
+        p = sbrk(size);
+        (*(uint32_t *)p) = size; 
+    }
+#ifdef DEBUG
+    max_size += size;
+    debug("alloc : size(%u) ptr(%p)\n", size, p);
+    debug("[=] heap max: %u\n\n", max_size);
+#endif
+    return p + 4;
 }
 
 void myfree(void *ptr)
 {
-    debug("free(%p)\n", ptr);
-    pushNode(ptr, *(uint32_t*)(ptr - 4) & 0xFFFFFFF8);
+    // printf("[myfree] ");
+#ifdef DEBUG
+    debug("[+] myfree(%p)\n", ptr);
+#endif
+    if (ptr == NULL)
+        return;
+    pushNode(ptr, (*(uint32_t *)(ptr - 4)) & 0xFFFFFFF8);
+#ifdef DEBUG
+    debug("[=] free : size(%u), ptr(%p)\n\n", (*(uint32_t *)(ptr - 4)) & 0xFFFFFFF8, (uint32_t *)(ptr - 4));
+#endif
+}
+
+void *myrealloc(void *ptr, uint32_t size)
+{
+    // printf("[myrealloc] ");
+#ifdef DEBUG
+    debug("[+] myrealloc : %d\n", size);
+#endif
+    void *p = myalloc(size);
+    if (ptr != NULL) {
+        if (p != NULL)
+            memcpy(p, ptr, size);
+        myfree(ptr);
+    }
+#ifdef DEBUG
+    debug("[=] realloc : oldptr(%p), size(%u), newptr(%p)\n\n", ptr, size, p);
+#endif
+    return p;
 }
